d9c74bbbea0b6910414ca8056ad9d8fb7a3f1310
lxc <1529184451@qq.com>
2022-01-20 11:00:55 +0800

    暂时移除 AspectJ

--- timer/library/src/main/java/com/jock/library/aop/aspectj/ActivityAspect.kt
+++ timer/library/src/main/java/com/jock/library/aop/aspectj/ActivityAspect.kt
@@ -12,15 +12,15 @@ import org.aspectj.lang.annotation.Aspect
  * Created by lxc on 1/8/22 5:51 PM.
  * Desc: Activity 切点
  */
-@Aspect
-class ActivityAspect {
-
-    @Around("execution(* android.app.Activity.setContentView(int))")
-    @Throws(Throwable::class)
-    fun onAround(joinPoint: ProceedingJoinPoint) {
-        val tag = "${joinPoint.target.classSimpleName()}.setContentView()"
-        startLifecycle(LifeStamp(tag))
-        joinPoint.proceed()
-        stopLifecycle(LifeStamp(tag))
-    }
-}
+//@Aspect
+//class ActivityAspect {
+//
+//    @Around("execution(* android.app.Activity.setContentView(int))")
+//    @Throws(Throwable::class)
+//    fun onAround(joinPoint: ProceedingJoinPoint) {
+//        val tag = "${joinPoint.target.classSimpleName()}.setContentView()"
+//        startLifecycle(LifeStamp(tag))
+//        joinPoint.proceed()
+//        stopLifecycle(LifeStamp(tag))
+//    }
+//}
